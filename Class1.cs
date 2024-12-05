09e89dfa9fc3a2637c64417bb7d9bc9144ea9ab9
PiotrM236 <moranskipiotr@gmail.com>
2024-12-05 11:43:21 +0100

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Obrazki
+{
+    public class Class1
+    {
+       
+
+        public string zmiana { get; set; }
+        public int polubienia { get; set; }
+
+        public Class1(string zmiana)
+        {
+            this.zmiana = zmiana;
+            polubienia = 0;
+        }
+
+      
+
+        public int polubienie()
+        {
+             return polubienia++;
+        }
+    }
+
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Obrazki
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public List<Class1> ryby = new List<Class1>();
+        int iterator = 0;
+        public MainWindow()
+        {
+            
+            
+            ryby.Add(new Class1("karp.jpg"));
+            ryby.Add(new Class1("sum.jpg"));
+            ryby.Add(new Class1("okon.jpg"));
+            InitializeComponent();
+            zmien();
+            
+        }
+
+        private void Button_click(object sender, RoutedEventArgs e)
+        {
+            if(iterator <= 0)
+            {
+                iterator = ryby.Count()-1;
+            }
+            else
+            {
+                iterator--;
+            }
+            zmien();
+
+        }
+
+        private void prawo_Click(object sender, RoutedEventArgs e)
+        {
+            if(iterator >= ryby.Count()-1)
+            {
+
+                iterator = 0;
+            }
+            else
+            {
+                iterator++;
+            }
+            zmien();
+
+        }
+
+        private void polubienia_Click(object sender, RoutedEventArgs e)
+        {
+            
+            zmien();
+
+
+
+        }
+        private void zmien()
+        {
+            obr_ryby.Source = new BitmapImage(new Uri(ryby[iterator].zmiana, UriKind.Relative));
+            blok.Text = ryby[iterator].polubienie().ToString();
+            
+        }
+    }
+}
